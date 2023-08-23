02067399f3b329c6eafef9ca40cc9ef9f90df34e
Rafael <rafael_ogando@bhdleon.com.do>
2023-08-23 18:25:38 -0400

    Deparment services

--- services/departamentosService.js
+++ services/departamentosService.js
@@ -1,6 +1,16 @@
 import axios from 'axios';
 
-export const getAllDepartamentos = () => {
-    const {data} = axios.get(`${process.env.PUBLIC_NEXT_API_HOST}/departamentos`);
-    return data
+export const GetDeparments = () => {
+    const {data} = axios.get(`${process.env.PUBLIC_NEXT_API_HOST}/api/departamentos`);
+    return data;
+}
+
+export const GetDepartamentbyId = (id) => {
+    const {data} = axios.get(`${process.env.PUBLIC_NEXT_API_HOST}/api/departamentos/${id}`);
+    return data;
+}
+
+export const CreateDepartament = (departamento) => {
+    const {data} = axios.post(`${process.env.PUBLIC_NEXT_API_HOST}/api/departamentos/`,departamento);
+    return data;
 }
