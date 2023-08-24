25f206e64ff50ee07cf0c9b96e5fc6baba60970a
Jeremy Mezon de la Cruz <51011294+JeremyMezon@users.noreply.github.com>
2023-08-23 21:12:00 -0400

    imp: auth init

--- services/autenticacionService.js
+++ services/autenticacionService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const GetAuthenticationToken = (loginInfo) => {
-    const {data} = axios.post(`${process.env.PUBLIC_NEXT_API_HOST}/api/auth`,loginInfo);
+export const GetAuthenticationToken = async (loginInfo) => {
+    const {data} = await axios.post(`${process.env.NEXT_PUBLIC_API_HOST}/api/auth`,loginInfo);
     return data;
 }
---
+++ shared/constants/auth.js
@@ -0,0 +1,5 @@
+export const AUTH_API = 
+    {
+        usuario: "Administrador",
+        clave: "admin123"
+      }
